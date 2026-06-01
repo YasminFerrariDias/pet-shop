0b97e55054d0111e8d40abafb2189f87c6df9425
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-06-01 14:22:01 -0300

    Criando Lógica para os Períodos

--- src/components/period-section/period-section.tsx
+++ src/components/period-section/period-section.tsx
@@ -1,25 +1,29 @@
+import { AppointmentPeriod } from "@/types/appointment";
 import { Cloudy, Moon, Sun } from "lucide-react"
 
 type PeriodSectionProps = {
-  period: any
+  period: AppointmentPeriod;
 }
 
 const periodIcons = {
   morning: <Sun className="text-accent-blue" />,
   afternoon: <Cloudy className="text-accent-orange" />,
-  everning: <Moon className="text-accent-yellow" />,
+  evening: <Moon className="text-accent-yellow" />,
 }
 
 export const PeriodSection = ({ period }: PeriodSectionProps) => {
   return (
     <section className="mb-8 bg-background-tertiary rounded-xl">
       <div className="flex items-center px-5 py-3 justify-between border-b border-[#2E2C30]">
-        <div>
+        <div className="flex items-center gap-2">
           {periodIcons[period?.type]}
           <h2 className="text-label-large-size text-content-primary">
             {period?.type}
           </h2>
         </div>
+        <span className="text-label-large-size text-content-secondary">
+          {period.timeRange}
+        </span>
       </div>
     </section>
   )
