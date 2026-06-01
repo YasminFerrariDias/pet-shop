e6e4202caa1a9e72a70ea28c395712ef8f994d2b
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-06-01 11:32:59 -0300

    Iniciando a Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
+        <div>
+          <h1 className="text-title text-content-primary mb-2">
+            Sua Agenda
+          </h1>
+          <p className="text-paragraph-small text-content-secondary">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje
+          </p>
+        </div>
+      </div>
+    </div >
   );
 }
